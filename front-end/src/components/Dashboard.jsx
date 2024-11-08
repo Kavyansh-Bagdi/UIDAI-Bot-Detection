87ee7ec3e9be9c193dbff633ed8abeff71d484b0
aabhinavvvvv <abhinavguptaxiia1443@gmail.com>
2024-11-08 16:59:04 +0530

    adding ux

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className='w-screen h-screen'>
+      <header className='flex gap-4 items-center justify-between w-3/5 mx-auto py-4 '>
+        <img src="src\assets\Emblem_of_India.svg.png" alt="" width={30} height={10}/>
+        <h1 className='text-2xl font-bold text-[#2072A0]'>Unique Identification Authority of India</h1>
+        <img src="src\assets\Page-1.svg" alt="" width={80} height={60}/>
+      </header>
         
       
     </div>

33645d493637cbe9c9e988bdc82e78df393739d7
aabhinavvvvv <abhinavguptaxiia1443@gmail.com>
2024-11-08 13:46:38 +0530

    adding login page

---
+++ front-end/src/components/Cards1.jsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import { FaFileUpload } from 'react-icons/fa';
+const Cards1 = () => {
+  return (
+    <div className='w-screen'>
+      <h1 className='text-2xl font-bold text-center text-[#2072A0]'>Services which require mobile number to be registered with Aadhaar</h1>
+    <div className='w-4/5 mx-auto h-full flex gap-9 flex-wrap mt-6 '>
+      <div>
+        <div>
+          <div className="max-w-xs w-[252px] p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center mb-4">
+          <img src="src\assets\Card1.svg" alt="" width={62.85} height={40}  className='bg-transparent'/>
+          </div>
+          <h2 className="text-xl font-bold text-gray-800 ">Document Update</h2>
+          <p className="text-gray-600">
+          Click Here to upload your Proof of Identity (PoI) and Proof of Address (PoA) Documents.
+          </p>
+          <p className="mt-3 text-red-600 font-semibold">
+          This service is free of cost till 14/12/2024.
+          </p>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <div>
+          <div className="max-w-xs w-[252px] p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center mb-4">
+          <img src="src\assets\Card1.svg" alt="" width={62.85} height={40}  className='bg-transparent'/>
+          </div>
+          <h2 className="text-xl font-bold text-gray-800 ">Document Update</h2>
+          <p className="text-gray-600">
+          Click Here to upload your Proof of Identity (PoI) and Proof of Address (PoA) Documents.
+          </p>
+          <p className="mt-3 text-red-600 font-semibold">
+          This service is free of cost till 14/12/2024.
+          </p>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <div>
+          <div className="max-w-xs w-[252px] p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center mb-4">
+          <img src="src\assets\Card1.svg" alt="" width={62.85} height={40}  className='bg-transparent'/>
+          </div>
+          <h2 className="text-xl font-bold text-gray-800 ">Document Update</h2>
+          <p className="text-gray-600">
+          Click Here to upload your Proof of Identity (PoI) and Proof of Address (PoA) Documents.
+          </p>
+          <p className="mt-3 text-red-600 font-semibold">
+          This service is free of cost till 14/12/2024.
+          </p>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <div>
+          <div className="max-w-xs w-[252px] p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center mb-4">
+          <img src="src\assets\Card1.svg" alt="" width={62.85} height={40}  className='bg-transparent'/>
+          </div>
+          <h2 className="text-xl font-bold text-gray-800 ">Document Update</h2>
+          <p className="text-gray-600">
+          Click Here to upload your Proof of Identity (PoI) and Proof of Address (PoA) Documents.
+          </p>
+          <p className="mt-3 text-red-600 font-semibold">
+          This service is free of cost till 14/12/2024.
+          </p>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <div>
+          <div className="max-w-xs w-[252px] p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center mb-4">
+          <img src="src\assets\Card1.svg" alt="" width={62.85} height={40}  className='bg-transparent'/>
+          </div>
+          <h2 className="text-xl font-bold text-gray-800 ">Document Update</h2>
+          <p className="text-gray-600">
+          Click Here to upload your Proof of Identity (PoI) and Proof of Address (PoA) Documents.
+          </p>
+          <p className="mt-3 text-red-600 font-semibold">
+          This service is free of cost till 14/12/2024.
+          </p>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <div>
+          <div className="max-w-xs w-[252px] p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center mb-4">
+          <img src="src\assets\Card1.svg" alt="" width={62.85} height={40}  className='bg-transparent'/>
+          </div>
+          <h2 className="text-xl font-bold text-gray-800 ">Document Update</h2>
+          <p className="text-gray-600">
+          Click Here to upload your Proof of Identity (PoI) and Proof of Address (PoA) Documents.
+          </p>
+          <p className="mt-3 text-red-600 font-semibold">
+          This service is free of cost till 14/12/2024.
+          </p>
+          </div>
+        </div>
+
+      </div>
+      <div>
+        <div>
+          <div className="max-w-xs w-[252px] p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center mb-4">
+          <img src="src\assets\Card1.svg" alt="" width={62.85} height={40}  className='bg-transparent'/>
+          </div>
+          <h2 className="text-xl font-bold text-gray-800 ">Document Update</h2>
+          <p className="text-gray-600">
+          Click Here to upload your Proof of Identity (PoI) and Proof of Address (PoA) Documents.
+          </p>
+          <p className="mt-3 text-red-600 font-semibold">
+          This service is free of cost till 14/12/2024.
+          </p>
+          </div>
+        </div>
+
+      </div>
+        
+
+    </div>
+
+    </div>
+        
+  )
+}
+
+export default Cards1
---
+++ front-end/src/components/Login.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+
+  return (
+    <div className=' border-b-2 w-full h-20 border-[#020b50]'>
+      {/* Header Component */}
+    <header className='flex gap-4 items-center justify-between w-[1170px] py-4 mx-auto '>
+        <img src="src\assets\loginlogo.svg" alt="" width={275} height={50} className='bg-cover'/>
+        <img src="src\assets\Page-1.svg" alt="" width={80} height={60} className='bg-cover'/>
+    </header>
+    {/* Form Button */}
+    <div className='w-full flex justify-center items-center '>
+      <form action="" className='w-full mt-5 bg-white p-4 rounded-md'>
+        <h1 className=' font-bold text-[#020b50] text-center text-xl'>Login to Aadhaar via OTP</h1>
+        <div className='gap-4 p-6 w-1/3 shadow-lg flex flex-col items-center justify-center h-56 mx-auto mt-5' style={{borderTop: '4px solid', borderImage: 'linear-gradient(to right, #020b50, #2C3A8F) 1'}}>
+          <input type="text" placeholder='Enter your Aadhaar Number' className='w-full p-3 rounded-md text-xl  outline-none border-b-2 border-black -mt-9 mb-7'/>
+          <button className='bg-gray-400 hover:bg-blue-900 hover:scale-105 transition-all duration-300 text-white p-2 rounded-md w-full'>Login With OTP</button>
+        </div>
+      </form>
+    </div>
+    <footer className='relative bottom-0 w-full flex justify-center items-center'>Copyright © 2024 Unique Identification Authority of India All Rights Reserved</footer>
+    </div>
+  );
+};
+
+export default Login;
